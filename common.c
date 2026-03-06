b284332430e9995b1e1c9bc4ba816c5695cf1ffc
Oshal Borkar <mail@oshalborkar.dev>
2026-03-06 12:59:54 +0530

    Added Exception handling and fixed minor issues

--- common.c
+++ common.c
@@ -67,7 +67,7 @@ void *memcpy(void *dst, const void *src, size_t n) {
   uint8_t *d = (uint8_t *) dst;
   const uint8_t *s = (const uint8_t *) src;
   while(n--)
-    *d-- = *s++;
+    *d++ = *s++;
   return dst;
 }
 
--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@ QEMU=/opt/local/bin/qemu-system-riscv32
 
 # Path to clang and compiler flags
 CC=/opt/local/bin/clang-mp-20
-CFLAGS="-std=c11 -O2 -g3 -Wall -Wextra --target=riscv32-unknown-elf -fno-stack-protector -ffreestanding -nostdlib"
+CFLAGS="-std=c11 -O2 -g3 -Wall -Wextra --target=riscv32-unknown-elf -fuse-ld=lld -fno-stack-protector -ffreestanding -nostdlib"
 
 # Build the kernel
 $CC $CFLAGS -Wl,-Tkernel.ld -Wl,-Map=kernel.map -o kernel.elf kernel.c common.c
